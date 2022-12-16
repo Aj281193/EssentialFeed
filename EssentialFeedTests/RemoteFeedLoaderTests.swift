c4d133ba7112734f756a3164112df4998021f1a3
Ashish Jaiswal <ashishjai28@gmail.com>
2022-12-17 01:29:56 +0530

    Remove default test File and create data from URL on load.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,18 +9,32 @@ import XCTest
 
 class RemoteFeedLoader {
     
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http:/a-u.com")
+    }
 }
 
 class HTTPClient {
+    static let shared  = HTTPClient()
+    private init() { }
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doestNotLoadDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
+    }
 }
+
