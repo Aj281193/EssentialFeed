e16e6fd750459c649dbd0d9851a55da827e7b729
Ashish Jaiswal <ashishjai28@gmail.com>
2022-12-17 01:45:52 +0530

    Guarantee we can load data twice.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,34 +8,38 @@
 import XCTest
 import EssentialFeed
 
-class RemoteFeedLoaderTests: XCTestCase {
-
-    func test_init_doestNotLoadDataFromURL() {
-       
-        let (_, client) = makeSut()
-        XCTAssertTrue(client.requestedURL.isEmpty)
-    }
-    
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "https://a-u.com")!
-        let (sut,client) = makeSut(url: url)
-        sut.load()
-        sut.load()
-        XCTAssertEqual(client.requestedURL,[url,url])
-    }
-    
-    // MARK:- Helpers
-    private func makeSut(url: URL =  URL(string: "https://a-u.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
-        return (sut, client)
-    }
-    
-    private class HTTPClientSpy: HTTPClient {
-         var requestedURL = [URL]()
-        func get(from url: URL) {
-            requestedURL.append(url)
-         }
-         
-     }
+func test_init_doestNotLoadDataFromURL() {
+   
+    let (_, client) = makeSut()
+    XCTAssertTrue(client.requestedURL.isEmpty)
 }
+
+func test_load_requestsDataFromURL() {
+    let url = URL(string: "https://a-u.com")!
+    let (sut,client) = makeSut(url: url)
+    sut.load()
+    XCTAssertEqual(client.requestedURL,[url])
+}
+
+func test_loadTwice_requestDataFromURLTwice() {
+    let url = URL(string: "https://a-u.com")!
+    let (sut,client) = makeSut(url: url)
+    sut.load()
+    sut.load()
+    XCTAssertEqual(client.requestedURL, [url,url])
+}
+
+// MARK:- Helpers
+private func makeSut(url: URL =  URL(string: "https://a-u.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    let client = HTTPClientSpy()
+    let sut = RemoteFeedLoader(url: url, client: client)
+    return (sut, client)
+}
+
+private class HTTPClientSpy: HTTPClient {
+     var requestedURL = [URL]()
+    func get(from url: URL) {
+        requestedURL.append(url)
+     }
+     
+ }
