483ae713293e215a8c260f1cf89af6e5dd2b3b24
Ashish Jaiswal <ashishjai28@gmail.com>
2022-12-17 23:13:51 +0530

    Deliver failure with error on request error also changes the name of enum  HttpResponseResult to HttpClientResult

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -151,13 +151,13 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
     
-        private var messages = [(url: URL, completion: (HTTPResponseResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
         
         var requestedURL: [URL] {
             return messages.map { $0.url }
         }
         
-        func get(from url: URL, completion: @escaping (HTTPResponseResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url,completion))
          }
          
--- Feed API/HTTPClient.swift
+++ Feed API/HTTPClient.swift
@@ -7,11 +7,11 @@
 
 import Foundation
 
-public enum HTTPResponseResult {
+public enum HTTPClientResult {
     case success(Data, HTTPURLResponse)
     case failure(Error)
 }
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPResponseResult) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
