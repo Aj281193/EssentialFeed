a4283b3a3e986f41e513d589095aa4aa92d9de80
Ashish Jaiswal <ashishjai28@gmail.com>
2022-12-22 19:16:00 +0530

    Finish URL loading when observing requests to make sure all URL request are finished before the method return, using dispatchQueue framework to read and write synchronously  on a centralised queue, this way we prevent data race condition

--- EssentialFeedTests/Feed API/URLSessionHttpClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHttpClientTests.swift
@@ -172,17 +172,31 @@ class URLSessionHttpClientTests: XCTestCase {
     
     private class URLProtocolStub: URLProtocol {
        
-        private static var stub: Stub?
-        private static var requestObserver: ((URLRequest) -> Void)?
+        private static var _stub: Stub?
+        private static var stub: Stub? {
+            get {
+                queue.sync {
+                    return _stub
+                }
+            }
+            set {
+                queue.sync {
+                    _stub = newValue
+                }
+            }
+        }
+       
+        private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
         
         private struct Stub {
             let data: Data?
             let response: URLResponse?
             let error: Error?
+            let requestObserver: ((URLRequest) -> Void)?
         }
         
         static func stub(data: Data?, response: URLResponse?, error: Error?) {
-            stub = Stub(data: data, response: response, error: error)
+            stub = Stub(data: data, response: response, error: error, requestObserver: nil)
         }
         
         static func startInterceptingRequest() {
@@ -190,7 +204,7 @@ class URLSessionHttpClientTests: XCTestCase {
         }
         
         static func observeRequests(observer: @escaping (URLRequest) -> Void) {
-            requestObserver = observer
+            stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
         }
         
         static func stopInterceptingRequest() {
@@ -208,23 +222,23 @@ class URLSessionHttpClientTests: XCTestCase {
         
         override func startLoading() {
           
-            if let requestObserver = URLProtocolStub.requestObserver {
-                client?.urlProtocolDidFinishLoading(self)
-                requestObserver(request)
-            }
+            guard let stub = URLProtocolStub.stub else { return }
             
-            if let data = URLProtocolStub.stub?.data {
+            if let data = stub.data {
                 client?.urlProtocol(self, didLoad: data)
             }
              
-            if let response = URLProtocolStub.stub?.response {
+            if let response = stub.response {
                 client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             }
             
-            if let error = URLProtocolStub.stub?.error {
+            if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
+            } else {
+                client?.urlProtocolDidFinishLoading(self)
             }
-            client?.urlProtocolDidFinishLoading(self)
+            
+            stub.requestObserver?(request)
         }
         
         override func stopLoading() { }
