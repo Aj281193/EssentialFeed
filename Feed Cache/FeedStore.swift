f1db4aa7d449d0e9501bd364ef676b076b0e73ba
Ashish Jaiswal <ashishjai28@gmail.com>
2022-12-30 20:20:32 +0530

    update the name of enum to RetrieveCaheFeedResult

--- Feed Cache/FeedStore.swift
+++ Feed Cache/FeedStore.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum RetrievalCompletionResult {
+public enum RetrieveCaheFeedResult {
     case empty
     case found(feed: [LocalFeedImage], timeStamp: Date)
     case failure(Error)
@@ -15,7 +15,7 @@ public enum RetrievalCompletionResult {
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (RetrievalCompletionResult) -> Void
+    typealias RetrievalCompletion = (RetrieveCaheFeedResult) -> Void
     
     func deleteCacheFeed(completion: @escaping DeletionCompletion)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
