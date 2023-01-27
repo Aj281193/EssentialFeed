0b005d3d0c07b2072a02f989b247d27b601b843e
Ashish Jaiswal <ashishjai28@gmail.com>
2023-01-27 19:02:54 +0530

    Extract system under test(sut) to factory method

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,26 +6,57 @@
 //
 
 import XCTest
+import EssentialFeed
 
-class FeedViewController {
-    var loader: FeedViewControllerTests.LoaderSpy
+final class FeedViewController: UIViewController {
+    private var loader: FeedLoader?
     
-    init(loader: FeedViewControllerTests.LoaderSpy) {
-        self.loader = loader
+     init(loader: FeedLoader) {
+         self.loader = loader
+         super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loader?.load() { _ in}
     }
 }
 
 final class FeedViewControllerTests: XCTestCase {
 
     func test_init_doesNotLoadFeed() {
-        let loader = LoaderSpy()
-        _ = FeedViewController(loader: loader)
+        let (_,loader) = makeSUT()
         
         XCTAssertEqual(loader.loadCallCount, 0)
     }
+    
+    func test_viewDidLoad_LoadsFeed() {
+        let (sut,loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
 
     //Mark:- Helpers
-    class LoaderSpy {
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        trackForMemoryLeak(sut,file: file,line: line)
+        trackForMemoryLeak(loader,file: file,line: line)
+        return (sut,loader)
+    }
+    
+    class LoaderSpy: FeedLoader {
         private(set) var loadCallCount = 0
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            loadCallCount += 1
+        }
     }
 }
