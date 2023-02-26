6637da6a0cd7e35dbaab1f7c2b68d118d75e3ad9
Ashish Jaiswal <ashishjai28@gmail.com>
2023-02-26 16:42:38 +0530

    Move UITableView helper to dequeue typed cell by class name

--- EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -50,9 +50,4 @@ final class FeedImageCellController: FeedImageView {
         cell = nil
     }
 }
-extension UITableView {
-    func dequeueReusableCell<T: UITableViewCell>() -> T {
-        let identifier = String(describing: T.self)
-        return dequeueReusableCell(withIdentifier: identifier) as! T
-    }
-}
+
