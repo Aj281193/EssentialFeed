45110bac4232c4ed7fc4bbe9ba671de41be7ad45
Ashish Jaiswal <ashishjai28@gmail.com>
2023-03-06 23:34:05 +0530

    Move FeedErrorView Model to separate file.

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Ashish Jaiswal on 06/03/23.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
