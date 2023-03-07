e1a55fd69bdb6c4ea34ceb165ee0e4625df1ef4e
Ashish Jaiswal <ashishjai28@gmail.com>
2023-03-07 23:44:59 +0530

    FeedPresenter display no error on `didStartLoadingFeed`

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,9 +7,26 @@
 
 import XCTest
 
+struct FeedErrorViewModel {
+    let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+}
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
 private class FeedPresenter {
-    init(view: Any) {
-        
+    private let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        errorView.display(.noError)
     }
 }
 
@@ -21,17 +38,34 @@ final class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.message.isEmpty, "Expected no view message")
     }
 
+    func test_didStartLoadingFeed_displayNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.message, [.display(errorMessage: .none)])
+    }
+    
     //MARK Helpers:-
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line)  -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
         
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(errorView: view)
         trackForMemoryLeak(sut,file: file,line: line)
         trackForMemoryLeak(view, file: file,line: line)
         return (sut,view)
     }
     
-    private class ViewSpy {
-        var message = [Any]()
+    private class ViewSpy: FeedErrorView {
+       
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        private(set) var message = [Message]()
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            message.append(.display(errorMessage: viewModel.message))
+        }
     }
 }
