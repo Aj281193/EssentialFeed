dea2a693e6bfa7f6bb35e3a209944f48f26f1ccc
Ashish Jaiswal <ashishjai28@gmail.com>
2023-03-09 23:51:46 +0530

    Move the initialalization of view and controller into a factory helper method.

--- EssentialFeedTests/FeedImage Presenter/FeedImagePresenterTests.swift
+++ EssentialFeedTests/FeedImage Presenter/FeedImagePresenterTests.swift
@@ -15,13 +15,22 @@ private class FeedImagePresenter {
 final class FeedImagePresenterTests: XCTestCase {
 
     func test_init_doesNotSendMessageToView() {
-        let view = ViewImageSpy()
-        
-        _ = FeedImagePresenter(view: view)
+        let (_,view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty, "Expected no view message")
     }
     
+    //MARK Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewImageSpy) {
+        
+        let view = ViewImageSpy()
+        let sut = FeedImagePresenter(view: view)
+        
+        trackForMemoryLeak(sut,file: file,line: line)
+        trackForMemoryLeak(view,file: file,line: line)
+        return (sut,view)
+    }
     
     private class ViewImageSpy {
         private(set) var messages = [Any]()
