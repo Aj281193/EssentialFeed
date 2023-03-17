58342ecdda697085085b61edb20409ab93b66ead
Ashish Jaiswal <ashishjai28@gmail.com>
2023-03-17 19:48:06 +0530

    Remove default URLSessionHTTPClient initialiser parameter(dependency) as client should be mindful  about which `URLSession` instance to use. For example, client may decide to use the .shared instance, when testing we ca inject a URLSession with an ephemeral configuration registered with the URLProtocol stub class.This way we don't  need to start or stop intercept request globally.

--- EssentialFeedTests/Feed API/URLSessionHttpClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHttpClientTests.swift
@@ -10,14 +10,9 @@ import EssentialFeed
 
 class URLSessionHttpClientTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        URLProtocolStub.startInterceptingRequest()
-    }
-    
     override func tearDown() {
         super.tearDown()
-        URLProtocolStub.stopInterceptingRequest()
+        URLProtocolStub.removeStub()
     }
     
     func test_getFromURL_performGETRequestWithURL() {
@@ -108,7 +103,10 @@ class URLSessionHttpClientTests: XCTestCase {
    // MARK:- Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
-        let sut = URLSessionHttpClient()
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let sut = URLSessionHttpClient(session: session)
         trackForMemoryLeak(sut,file: file,line: line)
         return sut
     }
@@ -204,6 +202,10 @@ class URLSessionHttpClientTests: XCTestCase {
             let response: URLResponse?
             let error: Error?
             let requestObserver: ((URLRequest) -> Void)?
+        }
+        
+        static func removeStub() {
+            stub = nil
         }
         
         static func stub(data: Data?, response: URLResponse?, error: Error?) {
