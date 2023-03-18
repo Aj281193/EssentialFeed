af63e5f55deac12eb894db6c8fa0c8fd3323432c
Ashish Jaiswal <ashishjai28@gmail.com>
2023-03-18 23:02:07 +0530

    Extract duplicate HTTPURLResponse.statusCode == 200 validation into a `isOK` helper method

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -48,7 +48,7 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
             task.completed(with: result.mapError { _ in Error.connectivity}.flatMap { (data, response) in
-                let isValidResponse = response.statusCode == 200 && !data.isEmpty
+                let isValidResponse = response.isOK && !data.isEmpty
                 
                 return isValidResponse ? .success(data) : .failure(Error.invalidData)
             })
