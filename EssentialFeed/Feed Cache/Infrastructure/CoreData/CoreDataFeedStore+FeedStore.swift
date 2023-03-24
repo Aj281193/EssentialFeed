561868bb9338e411936476cf9906f8f832c0935e
Ashish Jaiswal <ashishjai28@gmail.com>
2023-03-25 01:09:43 +0530

    Move CoreDataFeedStoreImplementation into separate file and group all coreData file into separate coreData group

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -1,22 +1,13 @@
 //
-//  CoreDataFeedStore.swift
+//  CoreDataFeedStore+FeedStore.swift
 //  EssentialFeed
 //
-//  Created by Ashish Jaiswal on 11/01/23.
+//  Created by Ashish Jaiswal on 25/03/23.
 //
 
-import CoreData
+import Foundation
 
-public final class CoreDataFeedStore: FeedStore {
-    
-    private let container: NSPersistentContainer
-    
-    private let context: NSManagedObjectContext
-    
-    public init(storeURL: URL, bundle: Bundle = .main) throws {
-        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
-        context = container.newBackgroundContext()
-    }
+extension CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion)  {
         perform { context in
@@ -47,9 +38,4 @@ public final class CoreDataFeedStore: FeedStore {
         }
     }
     
-    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
-        let context = self.context
-        context.perform { action(context) }
-    }
 }
-
---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Ashish Jaiswal on 11/01/23.
+//
+
+import CoreData
+
+public final class CoreDataFeedStore {
+    
+    private let container: NSPersistentContainer
+    
+    private let context: NSManagedObjectContext
+    
+    public init(storeURL: URL, bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
+        context = container.newBackgroundContext()
+    }
+    
+    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
+    }
+}
+
