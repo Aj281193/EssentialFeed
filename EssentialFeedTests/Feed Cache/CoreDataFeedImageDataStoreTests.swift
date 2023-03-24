d48c660ede729e4ac6b8ae28ead77cbaa10d7ae3
Ashish Jaiswal <ashishjai28@gmail.com>
2023-03-24 23:29:26 +0530

    Add a new FeedStore Core data model version adding an optional Data property to `ManagedFeedImage`

--- EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -26,6 +26,15 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: notFound(), for: anyURL())
     }
     
+    func test_retriveImageData_deliversNotFoundWhenStoreDataURLDoesNotMatch() {
+        let sut = makeSUT()
+        let url = URL(string: "http://a-url.com")!
+        let nonMatchingURL = URL(string: "https://another-url.com")!
+        
+        insert(anyData(), for: url, into: sut)
+        expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
+    }
+    
     // - MARK Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
@@ -50,6 +59,32 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
                 XCTAssertEqual(receivedData, expectedData,file: file,line: line)
             default:
                 XCTFail("expected result \(expectedResult) got received result \(receivedResult)",file: file,line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func localImage(url: URL) -> LocalFeedImage {
+        return LocalFeedImage(id: UUID(), description: "any", location: "any location", url: url)
+    }
+    
+ 
+    
+    private func insert(_ data: Data, for url: URL, into sut: CoreDataFeedStore, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for cache insertion")
+        let image = localImage(url: url)
+        sut.insert([image], timestamp: Date()) { result in
+            switch result {
+            case let .failure(error):
+                XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
+                
+            case .success:
+                sut.insert(data, for: url) { result in
+                    if case let Result.failure(error) = result {
+                        XCTFail("failed to insert data \(data) with \(error)",file: file,line: line)
+                    }
+                }
             }
             exp.fulfill()
         }
