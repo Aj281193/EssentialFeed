cfd66a029b186ff8020edb9b1186c99aab173500
Ashish Jaiswal <ashishjai28@gmail.com>
2023-04-02 16:11:11 +0530

    Add Empty EssentialApp Project into WorkSpace

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Ashish Jaiswal on 02/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
