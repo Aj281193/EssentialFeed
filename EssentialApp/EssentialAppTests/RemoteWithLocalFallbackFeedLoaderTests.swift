22d83d4a1557d8efd7d5b3183938ee2db8a2f746
Ashish Jaiswal <ashishjai28@gmail.com>
2023-04-07 09:21:30 +0530

    Extract SUT creation into a factory Method.

--- EssentialApp/EssentialAppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFallbackFeedLoaderTests.swift
@@ -24,12 +24,9 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
-        let primaryLoader = LoaderStub(.success(primaryFeed))
-        let fallbackLoader = LoaderStub(.success(fallbackFeed))
-        
-        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+       
         let exp = expectation(description: "Wait for load completion")
-        
+        let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
         sut.load { result in
             switch result {
             case let .success(receivedFeed):
@@ -42,9 +39,27 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         
         wait(for: [exp], timeout: 1)
     }
-
+    //MARK - Helpers
+    
+    private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result,file: StaticString = #filePath, line: UInt = #line) -> FeedLoader {
+        let primaryLoader = LoaderStub(primaryResult)
+        let fallbackLoader = LoaderStub(fallbackResult)
+        
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        trackForMemoryLeak(primaryLoader, file: file, line: line)
+        trackForMemoryLeak(fallbackLoader, file: file, line: line)
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+    
     private func uniqueFeed() -> [FeedImage] {
         return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "https://any-url.com")!)]
+    }
+    
+    func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have beeen deallocated, potential memory leak.",file: file, line: line)
+        }
     }
     
     private class LoaderStub: FeedLoader {
@@ -59,5 +74,6 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         }
     }
 }
+
 
 
