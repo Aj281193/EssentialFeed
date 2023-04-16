f6074e1fcebd000fb97c40a0708fb48b2212a297
Ashish Jaiswal <ashishjai28@gmail.com>
2023-04-16 08:45:59 +0530

    Add SceneDelegate configuration

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -21,33 +21,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        let session = URLSession(configuration: .ephemeral)
-        let remoteClient = URLSessionHttpClient(session: session)
-        let remoteFeedLoader = RemoteFeedLoader(url: url, client: remoteClient)
+    
+        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+
+        let remoteClient = URLSessionHttpClient(session: URLSession(configuration: .ephemeral))
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
 
         window?.rootViewController = FeedUIComposer.feedComposedWith(
             feedloader: remoteFeedLoader,
             imageLoader: remoteImageLoader)
-//        let localStorageURLs = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
-//
-//        let localStore = try! CoreDataFeedStore(storeURL: localStorageURLs)
-//        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
-//        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-//
-//        window?.rootViewController = FeedUIComposer.feedComposedWith(
-//            feedloader:
-//                FeedLoaderWithFallbackComposite(
-//                    primary: FeedLoaderCacheDecorator(
-//                        decoratee: remoteFeedLoader,
-//                        cache: localFeedLoader),
-//                    fallback: localFeedLoader),
-//            imageLoader: FeedImageDataLoaderWithFallbackComposite(
-//                primary: localImageLoader,
-//                fallback: FeedImageDataLoaderCacheDecorator(
-//                    decoratee: remoteImageLoader,
-//                    cache: localImageLoader)))
 
     }
 
