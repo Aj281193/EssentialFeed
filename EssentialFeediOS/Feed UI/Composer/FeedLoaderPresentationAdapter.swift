bcc583ebfa63234f87695502826836d7e8a51973
Ashish Jaiswal <ashishjai28@gmail.com>
2023-04-20 20:27:26 +0530

    separate Each class into Separate File in FeedUI composer group

---
+++ EssentialFeediOS/Feed UI/Composer/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,32 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by Ashish Jaiswal on 20/04/23.
+//
+
+import UIKit
+import EssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
