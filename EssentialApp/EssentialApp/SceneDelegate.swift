ca387fe46a8dfff7d14f654826baf9fd6af1c5d0
Ashish Jaiswal <ashishjai28@gmail.com>
2023-07-02 16:45:37 +0530

    Remove unused debugging code that was previously used during UI tests

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -13,13 +13,12 @@ import CoreData
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    let localStorageURLs = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
-
+  
     private lazy var httpClient: HTTPClient = {
         URLSessionHttpClient(session: URLSession(configuration: .ephemeral))
     }()
     private lazy var store: FeedStore & FeedImageDataStore = {
-        try! CoreDataFeedStore(storeURL: localStorageURLs)
+        try! CoreDataFeedStore(storeURL: NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite"))
     }()
     
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
@@ -40,7 +39,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func configureWindow() {
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
 
-        let remoteClient = makeRemoteClient()
+        let remoteClient = httpClient
         let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
 
@@ -63,9 +62,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     }
 
-    func makeRemoteClient() -> HTTPClient {
-        return httpClient
-    }
+  
     
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
