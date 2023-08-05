442d2e04bc7a7d25a3a7f95000f6b9e1d78996af
Ashish Jaiswal <ashishjai28@gmail.com>
2023-08-05 19:21:43 +0530

    Extract shared ListViewController tests

--- EssentialFeediOSTests/FeedUI/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedUI/FeedSnapshotTests.swift
@@ -11,31 +11,12 @@ import EssentialFeediOS
 
 final class FeedSnapshotTests: XCTestCase {
     
-    func test_emptyFeed() {
-        let sut = makeSUT()
-        sut.display(emptyFeed())
-        
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
-        
-    }
-    
-    
     func test_feedWithContent() {
         let sut = makeSUT()
         sut.display(feedWithContent())
         
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_CONTENT_dark")
-    }
-    
-    func test_feedWithErrorMessage() {
-        let sut = makeSUT()
-        
-        sut.display(.error(message: "This is a \n multi-line \n error message"))
-        
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
     }
     
     func test_feedWithFailedImageLoading() {
@@ -72,11 +53,7 @@ final class FeedSnapshotTests: XCTestCase {
             )
         ]
     }
-    
-    private func emptyFeed() -> [FeedImageCellController] {
-        return []
-    }
-    
+        
     private func feedWithFailedImageLoading() -> [ImageStub] {
         return [
             ImageStub(
