f63ac4eafee3856d04c3d33017970fb02f607556
Ashish Jaiswal <ashishjai28@gmail.com>
2023-08-09 11:32:34 +0530

    Revert "update ListSnapshot test helper"
    This reverts commit 21a818cef794a9f88bd960e95a8afb74bb96b129.

--- EssentialFeediOSTests/SharedUI/ListSnapshotTests.swift
+++ EssentialFeediOSTests/SharedUI/ListSnapshotTests.swift
@@ -32,9 +32,10 @@ final class ListSnapshotTests: XCTestCase {
     
     //MARK:- Helpers
     private func makeSUT() -> ListViewController {
-        let controller = ListViewController()
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
         controller.loadViewIfNeeded()
-        controller.tableView.separatorStyle = .none
         controller.tableView.showsVerticalScrollIndicator = false
         controller.tableView.showsHorizontalScrollIndicator = false
         return controller
