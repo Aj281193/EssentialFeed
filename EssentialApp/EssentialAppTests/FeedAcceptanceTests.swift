8a20cf5bcf9e3c74c5586d253ed96abe5ce23acd
Ashish Jaiswal <ashishjai28@gmail.com>
2023-08-15 15:59:35 +0530

    Show comments on Image Selection

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -41,19 +41,6 @@ final class FeedAcceptanceTests: XCTestCase {
         XCTAssertEqual(feed.numbderOfRenderFeedImageView(), 0)
     }
     
-    //MARK: - Helpers
-    
-    private func launch(httpClient: HTTPClientStub = .offline, store: InMemoryFeedStore = .empty) -> ListViewController {
-        
-        let sut = SceneDelegate(httpClient: httpClient, store: store)
-        
-        sut.window = UIWindow()
-        sut.configureWindow()
-        
-        let nav = sut.window?.rootViewController as? UINavigationController
-        return nav?.topViewController as! ListViewController
-    }
-    
     func test_onEnteringBackground_deleteExpiredFeedCache() {
         let store = InMemoryFeedStore.withExpiredFeedCache
         
@@ -70,6 +57,37 @@ final class FeedAcceptanceTests: XCTestCase {
         XCTAssertNotNil(store.feedCache,"Expected to keep non-expired cache")
     }
     
+    func test_onFeedImageSelection_displaysComments() {
+        let comments = showCommentsForFirstImage()
+        
+        XCTAssertEqual(comments.numberOfRenderComments(), 1)
+        XCTAssertEqual(comments.commentMessage(at: 0), makeCommentMessage())
+    }
+    
+    
+    //MARK: - Helpers
+    
+    private func launch(httpClient: HTTPClientStub = .offline, store: InMemoryFeedStore = .empty) -> ListViewController {
+        
+        let sut = SceneDelegate(httpClient: httpClient, store: store)
+        
+        sut.window = UIWindow()
+        sut.configureWindow()
+        
+        let nav = sut.window?.rootViewController as? UINavigationController
+        return nav?.topViewController as! ListViewController
+    }
+    
+    private func showCommentsForFirstImage() -> ListViewController {
+        let feed = launch(httpClient: .online(response), store: .empty)
+        
+        feed.simulateOnTapFeedImage(at: 0)
+        RunLoop.current.run(until: Date())
+        
+        let nav = feed.navigationController
+        return nav?.topViewController as! ListViewController
+    }
+
     private func  enterBackground(with store: InMemoryFeedStore) {
         let sut = SceneDelegate(httpClient: HTTPClientStub.offline, store: store)
         sut.sceneWillResignActive(UIApplication.shared.connectedScenes.first!)
@@ -160,7 +178,8 @@ final class FeedAcceptanceTests: XCTestCase {
             return makeImageData()
         case "/essential-feed/v1/feed":
             return makeFeedData()
-            
+        case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
+            return makeCommentsData()
         default:
             return Data()
         }
@@ -176,6 +195,21 @@ final class FeedAcceptanceTests: XCTestCase {
             ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-2"]
         ]])
     }
+    
+    private func makeCommentsData() -> Data {
+           return try! JSONSerialization.data(withJSONObject: ["items": [
+               [
+                   "id": UUID().uuidString,
+                   "message": makeCommentMessage(),
+                   "created_at": "2020-05-20T11:24:59+0000",
+                   "author": [
+                       "username": "a username"
+                   ]
+               ] as [String : Any]
+           ]])
+       }
    
-
+    private func makeCommentMessage() -> String {
+        "a message"
+    }
 }
