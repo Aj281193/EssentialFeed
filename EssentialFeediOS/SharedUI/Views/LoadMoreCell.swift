54616cb7899544d6848421d17cc7ba67cbac5388
Ashish Jaiswal <ashishjai28@gmail.com>
2023-09-01 16:41:03 +0530

    Add "Load More" message Label

--- EssentialFeediOS/SharedUI/Views/LoadMoreCell.swift
+++ EssentialFeediOS/SharedUI/Views/LoadMoreCell.swift
@@ -9,6 +9,25 @@ import UIKit
 
 public class LoadMoreCell: UITableViewCell {
     
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .tertiaryLabel
+        label.font = .preferredFont(forTextStyle: .footnote)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            contentView.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: 8),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            contentView.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 8)
+        ])
+        return label
+    }()
+    
     private lazy var spinner: UIActivityIndicatorView = {
         
         let spinner = UIActivityIndicatorView(style: .medium)
@@ -25,7 +44,7 @@ public class LoadMoreCell: UITableViewCell {
         return spinner
     }()
     
-    var isLoading: Bool {
+    public var isLoading: Bool {
         get { spinner.isAnimating }
         set {
             if newValue {
@@ -34,5 +53,10 @@ public class LoadMoreCell: UITableViewCell {
                 spinner.stopAnimating()
             }
         }
+    }
+    
+    public var message: String? {
+        get { messageLabel.text }
+        set { messageLabel.text = newValue }
     }
 }
