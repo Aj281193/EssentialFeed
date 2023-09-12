92320f60dcb5f72d2596e974becb522f2e6a571a
Ashish Jaiswal <ashishjai28@gmail.com>
2023-09-12 22:38:34 +0530

    Add helper method to find imageData for a given URL

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -22,6 +22,11 @@ class ManagedFeedImage: NSManagedObject {
 }
 extension ManagedFeedImage {
     
+    static func data(with url: URL, in context: NSManagedObjectContext) throws -> Data? {
+        
+       return try ManagedFeedImage.first(with: url, in: context)?.data
+    }
+    
     static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
         let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
         request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
